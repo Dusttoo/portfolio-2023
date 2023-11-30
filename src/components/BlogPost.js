367f4196c38194bffb14fe1a8e3c070e39b492b4
Dusty Mumphrey <dusty@clockwork.com>
2023-11-30 08:22:05 -0600

    added content to blog post page

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -1,10 +1,14 @@
+// BlogPost.js
 import React, { useEffect, useState } from "react";
 import { contentfulClient } from "../services/Contentful";
 import { useParams } from "react-router-dom";
+import "../styles/BlogPost.css"; // Ensure the path to your CSS file is correct
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 const BlogPost = () => {
   const { postId } = useParams();
   const [post, setPost] = useState(null);
+  console.log(post)
 
   useEffect(() => {
     contentfulClient
@@ -16,14 +20,27 @@ const BlogPost = () => {
   }, [postId]);
 
   if (!post) {
-    return <div>Loading...</div>;
+    return <div className="blog-post-container">Loading...</div>;
   }
 
+    const renderContent = (content) => {
+      // If the content from Contentful is rich text, it needs to be rendered as such
+      return documentToReactComponents(content);
+    };
+
   return (
-    <article>
-      <h1>{post.title}</h1>
-      {/* ... display other parts of the blog post */}
-    </article>
+    <div className="blog-post-container">
+      <h1 className="blog-post-title">{post.title}</h1>
+      {post.featuredImage && post.featuredImage.fields.file.url && (
+        <img
+          className="blog-post-image"
+          src={post.featuredImage.fields.file.url}
+          alt={post.title}
+        />
+      )}
+      <div className="blog-post-content">{renderContent(post.content)}</div>
+      {/* Add any additional post details you want to include */}
+    </div>
   );
 };
 
